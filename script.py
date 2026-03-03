6b602410b3882a7bdb9eb0f86f0c822cd3284e92
kat <177570715+kxttykat@users.noreply.github.com>
2026-03-03 01:30:23 +0000

    refactored script

--- script.py
+++ script.py
@@ -1,133 +1,93 @@
+import os
 import urllib.request
 import subprocess
-import re
-import os
+from colorama import init, Fore
 from time import sleep
 
-# testing code server
+init()
 
 images = {
-    'Ubuntu 24.04': 'https://cloud-images.ubuntu.com/minimal/releases/noble/release/',
-    'Ubuntu 22.04': 'https://cloud-images.ubuntu.com/minimal/releases/jammy/release/'
+    'Debian 13 Trixie': 'https://cdimage.debian.org/cdimage/cloud/trixie/latest/debian-13-generic-amd64.qcow2',
+    'Ubuntu 24.04 Noble': 'https://cloud-images.ubuntu.com/minimal/releases/noble/release/ubuntu-24.04-minimal-cloudimg-amd64.img'
 }
 
-def img_ext(base_url):
-    with urllib.request.urlopen(base_url) as response:
-        html = response.read().decode('utf-8')
-        match = re.search(r'href="([^"]*amd64\.img)"', html)
-        if match:
-            return base_url + match.group(1)
-        else:
-            return None
+class Installer:
+    def __init__(self):
+        self.file = None
 
-def setup():
-    img_options = {
-        '1': 'Ubuntu 24.04',
-        '2': 'Ubuntu 22.04'
-    }
-    print('''What image would you like to select?
-[1] Ubuntu 24.04
-[2] Ubuntu 22.04''')
-    img = input('Enter Number: ')
-    selected_image = img_options.get(img)
-    if selected_image:
+    def download(self):
         os.system('clear')
-        url = img_ext(images[selected_image])
-        filename = url.split('/')[-1]
-        print('Downloading...')
-        urllib.request.urlretrieve(url, filename)
-        os.system('clear')
-        print('Done!')
-        sleep(2)
-        configure(filename)
-    else:
-        os.system('clear')
-        print('unrecognized input...')
-        sleep(2)
-        os.system('clear')
-        setup()
-
-def configure(filename):
-    os.system('clear')
-    print(f'Starting configuration on {filename}...')
-    sleep(3)
-    os.system('clear')
-    os.system(f'mv {filename}.img {filename}.qcow2')
-    print(filename + ' has been configured!')
-    sleep(3)
-    os.system('clear')
-    img_resize = input('What would you like to resize the images disk to (Gigabytes): ')
-    os.system(f'qemu-img resize {filename} {img_resize}G')
-    print('libguestfs-tools must be installed for some parts of the configuration. Do u want to install it?')
-    libguestfs = input('(Y/N): ')
-    if libguestfs.lower() in ['y', 'yes']:
-        try:
+        print('''Which cloud image would you like to install?
+Debian 13 Trixie: 1
+Ubuntu 24.04 Noble: 2''')
+        choice = input("Enter A Number: ")
+        if choice == '1':
             os.system('clear')
-            subprocess.run(['apt', 'install', 'libguestfs-tools', '-y'], check=True)
-        except subprocess.CalledProcessError as err:
-            print(f'unable to install libguestfs-tools: {err}')
-            return
-    else:
-        print('libguestfs-tools is a requirement. exiting script...')
-        return
-    try:
-        subprocess.run(['virt-customize', '-a', filename, '--run-command', '"sudo echo > /etc/machine id; sudo ln -sf /etc/machine-id /var/lib/dbus/machine-id"'], check=True)
-    except subprocess.CalledProcessError as err:
-        print(f'An error occured with the machine ID: {err}')
-        return
-    vmid = input('Enter Virtual Machine ID: ')
-    try:
-        subprocess.run(['qm', 'set', f'{vmid}', '--serial0', 'socket', '--vga', 'serial0'], check=True)
-    except subprocess.CalledProcessError as err:
-        print(f'Invalid virtual machine ID: {err}')
-        return
+            print(Fore.GREEN + 'Downloading...')
+            self.file = urllib.request.urlretrieve(images["Debian 13 Trixie"], 
+            "debian-13-generic-amd64.qcow2")
+            os.system('clear')
+            print(Fore.BLUE + f'Download has finished for {self.file[0]}!')
+            sleep(3)
+        elif choice == '2':
+            os.system('clear')
+            print(Fore.GREEN + 'Downloading...')
+            self.file = urllib.request.urlretrieve(images["Ubuntu 24.04 Noble"], 
+            "ubuntu-24.04-minimal-cloudimg-amd64.img")
+            os.system('clear')
+            print(Fore.BLUE + f'Download has finished for {self.file[0]}!')
+            sleep(3)
+        else:
+            print(Fore.RESET + 'You did not specify an image! returning to menu...')
+            sleep(3)
+            download()
 
-    root_pwd = input('Would you like to set a root password? (Y/N): ')
-    if root_pwd.lower() in ['y', 'yes']:
+    def setup(self):
+        os.system('clear')
+        img_resize = input('Please specify a size to resize the image with(E.G: 8GB,16GB,32GB): ')
         try:
-            pwd = input('What would you like the root password to be: ')
-            subprocess.run(['virt-customize', '-a', filename, '--root-password', f'password:{pwd}'], check=True)
-        except subprocess.CalledProcessError as err:
-            print(f'An error occurred with the root password: {err}')
-            return
-    else:
-        pass
-    print('Setting a timezone for the image (ex: Europe/London, America/New_York')
-    try:
-        timezone = input('What timezone would you like the image to be in: ')
-        subprocess.run(['virt-customize', '-a', filename, '--timezone', timezone], check=True)
-    except subprocess.CalledProcessError as err:
-        print(f'An error occured with the timezone: {err}')
-        return
-    ssh_config = input('Would you like to configure SSH? (Y/N):')
-    if ssh_config.lower() in ['y', 'yes']:
-        try:
-            subprocess.run(['virt-edit', filename, '/etc/sshd_config.d/60_cloudimg-settings.conf'], check=True)
-            subprocess.run(['virt_edit', filename, '/etc/ssh/sshd_config.d/sshd_config'], check=True)
-        except subprocess.CalledProcessError as err:
-            print(f'An error occurred with the ssh configuration: {err}')
-            return
-    pkgs = input('Would you like to install any external packages? (Y/N):')
-    if pkgs.lower() in ['y', 'yes']:
-        try:
-            pkgs = input('What packages would you like to install on the image: ')
-            pkgs = ','.join(pkgs.replace(',', ' ').split()) # unsure if this is doing it's intended behaviour
-            subprocess.run([f'virt-customize', '-a', '{filename}', f'--install {pkgs}'], check=True)
-        except subprocess.CalledProcessError as err:
-            print(f'Invalid packages: {err}')
-            return
-    else:
-        pass
-    finished = input('Would you like to finish the install and import the image into the VM? (Y/N):')
-    if finished.lower() in ['y', 'yes']:
-        try:
-            drive_name = input('What drive does ur VMs disk sit on (example: local-lvm: ')
-            subprocess.run(['qm', 'importdisk', vmid, filename, drive_name, '--format', 'qcow2'], check=True)
-        except subprocess.CalledProcessError as err:
-            print(f'An error occurred with the drive name: {err}')
-            return
-    else:
-        print('exiting script... you can always import it later!')
+            subprocess.run(f'qemu-img resize {self.file[0]} {img_resize}',
+            shell=True,
+            text=True)
+        except Exception as e:
+            print(e)
+        os.system('clear')
+        pkgs = input(Fore.RESET + 'Do you want to install packages to this image?(y/N): ')
+        if pkgs.lower() in ['y', 'yes']:
+            os.system('clear')
+            packages = input("Please enter packages to install(format: pkg1,pkg2,pkg3): ")
+            try:
+                subprocess.run(f"virt-customize -a {self.file[0]} --install {packages}", 
+                shell=True,
+                text=True)
+                os.system('clear')
+                print(Fore.BLUE + f'Packages have finished installing for {self.file[0]}')
+            except Exception as e:
+                print(e)
+        else:
+            print('exiting.')
+            sleep(1)
+            os.system('clear')
+            print('exiting..')
+            sleep(1)
+            os.system('clear')
+            print('exiting...')
+        os.system('clear')
+        print('Giving image new machine-id.')
+        os.system('clear')
+        sleep(1)
+        print('Giving image new machine-id..')
+        os.system('clear')
+        sleep(1)
+        os.system('clear')
+        print(Fore.GREEN + 'Giving image new machine-id...')
+        subprocess.run(f'virt-customize -a {self.file[0]} --run-command "truncate -s 0 /etc/machine-id"', 
+        shell=True,
+        text=True)
+        os.system('clear')
+        print(Fore.BLUE + 'Done!')
 
 if __name__ == '__main__':
-    setup()
+    installer = Installer()
+    installer.download()
+    installer.setup()
